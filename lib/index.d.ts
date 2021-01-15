0dac557f31ad58126c5f7a498cab35bb1d27f988
Calbabreaker <calbabreaker@gmail.com>
2021-01-15 16:47:53 +1030

    build and bump version to 1.1.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -15,7 +15,7 @@ export declare class ValidationChainer<ObjType> {
     errors?: ValidationError[];
     constructor(objToValidate: ObjType);
     /**
-     * Starts a new validation call stack with the property.
+     * Selects a property to start checking. All following validate, sanitize, ensure calls will be on this property until the next check call.
      *
      * @param propertyKey - The property to use.
      * @returns The validation chainer (this object) to chain.
@@ -23,31 +23,31 @@ export declare class ValidationChainer<ObjType> {
     check(propertyKey: keyof ObjType): ValidationChainer<ObjType>;
     /**
      * Takes in a function to check if the property was valid and shows the message in the errors when not.
-     * It will stop the current validation call stack if it failed.
+     * It will stop checking the selected property if it failed.
      *
-     * @param func - A function that returns whether or not the property was valid. It can be a promise.
+     * @param func - A function that takes the selected property value and returns whether or not the property was valid. It can be a promise.
      * @param message - The message to show in the errors when the property fails validation.
      * @returns The validation chainer (this object) to chain.
      */
-    validate<T = never>(func: (value: T) => Promise<boolean> | boolean, message?: string): ValidationChainer<ObjType>;
+    validate<T = any>(func: (value: T) => Promise<boolean> | boolean, message?: string): ValidationChainer<ObjType>;
     /**
      * Replaces the property value with whatever the function returns.
      *
-     * @param func - A function to replace the property value. It can be a promise.
+     * @param func - A function that takes the selected property value and returns the replaced value. It can be a promise.
      * @returns The validation chainer (this object) to chain.
      */
-    sanitize<T = never>(func: (value: T) => Promise<T> | T): ValidationChainer<ObjType>;
+    sanitize<T = any>(func: (value: T) => Promise<T> | T): ValidationChainer<ObjType>;
     /**
-     * Fails the property if the previously checked property that is passed in has failed.
+     * Takes in a previously checked property and fails the selected property if that property had failed.
      *
-     * @param propertyKey - The property to check
+     * @param propertyKey - The property to check if it had failed
      * @param message - The message to show in the errors when it failed. Leave this blank to use the failed property's message.
      * @returns The validation chainer (this object) to chain.
      */
-    ensureProperty(propertyKey: keyof ObjType, message?: string): ValidationChainer<ObjType>;
+    ensure(propertyKey: keyof ObjType, message?: string): ValidationChainer<ObjType>;
     /**
      * The function to call at the end of the chain.
-     * This is will start executing the functions in the callstacks.
+     * This will start executing the validate, sanitize and or ensure calls.
      *
      * @returns A promise that resolves to an array of ValidationErrors. It's a promise because the validation functions might contain promises.
      */
